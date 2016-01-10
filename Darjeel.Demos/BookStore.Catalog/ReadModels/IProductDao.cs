621bb001f5003606e527bdc8482210da23eda827
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:23:05 +0100

    Added the product DAO interface for the book store demo

---
+++ Darjeel.Demos/BookStore.Catalog/ReadModels/IProductDao.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStore.Catalog.ReadModels
+{
+    public interface IProductDao
+    {
+        Task<IEnumerable<IProduct>> FindAsync();
+    }
+}
