290c6edb45bc834fcdf0dc4cf7f2420deb52fb5f
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:39:17 +0100

    Added the product DAO implementation for the book store demo

---
+++ Darjeel.Demos/BookStore.Catalog.Persistence/ReadModels/ProductDao.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Catalog.ReadModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Catalog.Persistence.ReadModels
+{
+    public class ProductDao : IProductDao
+    {
+        private readonly IReadModelContext _context;
+
+        public ProductDao(IReadModelContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            _context = context;
+        }
+
+        public async Task<IEnumerable<IProduct>> FindAsync()
+        {
+            var query = from x in _context.Products
+                        select x;
+            var products = await query.ToArrayAsync();
+
+            return products;
+        }
+    }
+}
