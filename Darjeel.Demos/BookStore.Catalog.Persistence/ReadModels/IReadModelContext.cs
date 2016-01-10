d3e48ea5248c1b189178ad8427b4b435b27e58a8
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:30:41 +0100

    Added the read model context interface

---
+++ Darjeel.Demos/BookStore.Catalog.Persistence/ReadModels/IReadModelContext.cs
@@ -0,0 +1,10 @@
+﻿using Darjeel.Infrastructure.EntityFramework;
+using System.Data.Entity;
+
+namespace BookStore.Catalog.Persistence.ReadModels
+{
+    public interface IReadModelContext : IContext
+    {
+        IDbSet<Product> Products { get; }
+    }
+}
