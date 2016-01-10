32a0b73e07b2430b2c600bf6a8931d620339f747
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:19:52 +0100

    Added the read model product interface for the book store demo

---
+++ Darjeel.Demos/BookStore.Catalog/ReadModels/IProduct.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BookStore.Catalog.ReadModels
+{
+    public interface IProduct
+    {
+        Guid AggregateId { get; set; }
+        string Title { get; set; }
+    }
+}
