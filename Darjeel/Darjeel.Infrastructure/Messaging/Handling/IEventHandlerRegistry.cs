68ef9ab27de195e214e1c7b1fdcd19c47b8f2b7f
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 14:48:22 +0100

    Added the event handler registry interface

---
+++ Darjeel/Darjeel.Infrastructure/Messaging/Handling/IEventHandlerRegistry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Darjeel.Infrastructure.Messaging.Handling
+{
+    public interface IEventHandlerRegistry : IRegistry<IEventHandler>
+    {
+        bool TryGetHandlers(Type eventType, out IEnumerable<IEventHandler> handlers);
+    }
+}
