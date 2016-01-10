2704fac2258816772b9cdbb40de09f492f466841
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 14:22:17 +0100

    Added the event sourced interface

---
+++ Darjeel/Darjeel.Infrastructure/EventSourcing/IEventSourced.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Darjeel.Infrastructure.EventSourcing
+{
+    public interface IEventSourced
+    {
+        int Version { get; }
+        IEnumerable<IVersionedEvent> UncommittedEvents { get; }
+    }
+}
