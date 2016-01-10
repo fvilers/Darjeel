3f16565aca96b56760bca2d68593a1b72bc02791
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 14:20:50 +0100

    Added a base versioned event implementation

---
+++ Darjeel/Darjeel.Infrastructure/EventSourcing/VersionedEvent.cs
@@ -0,0 +1,17 @@
+﻿using Darjeel.Infrastructure.Messaging;
+using System;
+
+namespace Darjeel.Infrastructure.EventSourcing
+{
+    public abstract class VersionedEvent : Event, IVersionedEvent
+    {
+        public int Version { get; }
+
+        protected VersionedEvent(Guid sourceId, int version)
+            : base(sourceId)
+        {
+            if (version < 0) throw new ArgumentOutOfRangeException(nameof(version));
+            Version = version;
+        }
+    }
+}
