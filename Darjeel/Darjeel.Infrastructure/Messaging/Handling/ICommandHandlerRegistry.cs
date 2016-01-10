bcb329e96b7ee37c3b3d4c15b6f9083ae4573cbf
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 14:42:53 +0100

    Added the command handler registry interface

---
+++ Darjeel/Darjeel.Infrastructure/Messaging/Handling/ICommandHandlerRegistry.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Darjeel.Infrastructure.Messaging.Handling
+{
+    public interface ICommandHandlerRegistry : IRegistry<ICommandHandler>
+    {
+        bool TryGetHandler(Type commandType, out ICommandHandler handler);
+    }
+}
