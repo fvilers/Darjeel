5cab9053bfb1a3b9af75d9a2104cdf6f46d1a335
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:52:37 +0100

    Added the processor interface

---
+++ Darjeel/Darjeel.Infrastructure/Processors/IProcessor.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+
+namespace Darjeel.Infrastructure.Processors
+{
+    public interface IProcessor
+    {
+        void Start(CancellationToken cancellationToken);
+    }
+}
