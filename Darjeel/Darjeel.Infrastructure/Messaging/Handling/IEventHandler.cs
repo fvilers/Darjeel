c0d2ee9fcb4a3ad9e586df3df70051f09c288fbc
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 14:13:47 +0100

    Added the event handler interface

---
+++ Darjeel/Darjeel.Infrastructure/Messaging/Handling/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Darjeel.Infrastructure.Messaging.Handling
+{
+    public interface IEventHandler
+    {
+    }
+
+    public interface IEventHandler<in T> : IEventHandler
+        where T : IEvent
+    {
+        Task HandleAsync(T @event);
+    }
+}
