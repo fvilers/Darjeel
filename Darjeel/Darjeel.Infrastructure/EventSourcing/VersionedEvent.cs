ba00628504ca03068fa8196ce2950081079c92d3
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 14:30:14 +0100

    Added the base aggregate root implementation

--- Darjeel/Darjeel.Infrastructure/EventSourcing/VersionedEvent.cs
+++ Darjeel/Darjeel.Infrastructure/EventSourcing/VersionedEvent.cs
@@ -3,15 +3,18 @@ using System;
 
 namespace Darjeel.Infrastructure.EventSourcing
 {
-    public abstract class VersionedEvent : Event, IVersionedEvent
+    public class VersionedEvent : Event, IVersionedEvent
     {
         public int Version { get; }
+        public IEvent Event { get; }
 
-        protected VersionedEvent(Guid sourceId, int version)
+        public VersionedEvent(Guid sourceId, int version, IEvent @event)
             : base(sourceId)
         {
             if (version < 0) throw new ArgumentOutOfRangeException(nameof(version));
+            if (@event == null) throw new ArgumentNullException(nameof(@event));
             Version = version;
+            Event = @event;
         }
     }
 }
