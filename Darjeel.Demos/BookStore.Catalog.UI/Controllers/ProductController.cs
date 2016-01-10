1284f284eaab0a23bbe75a7041b21e19e7a6d957
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:17:07 +0100

    Added the product controller to the book store demo

---
+++ Darjeel.Demos/BookStore.Catalog.UI/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace BookStore.Catalog.UI.Controllers
+{
+    [RoutePrefix("api/catalog/products")]
+    public class ProductController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Find()
+        {
+            return Ok();
+        }
+    }
+}
