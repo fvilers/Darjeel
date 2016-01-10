a2a3ae76bf6ba640439b5a661710c0b7dba0c0f4
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:21:36 +0100

    Added the read model product implementation for the book store demo

---
+++ Darjeel.Demos/BookStore.Catalog.Persistence/ReadModels/Product.cs
@@ -0,0 +1,11 @@
+﻿using BookStore.Catalog.ReadModels;
+using System;
+
+namespace BookStore.Catalog.Persistence.ReadModels
+{
+    public class Product : IProduct
+    {
+        public Guid AggregateId { get; set; }
+        public string Title { get; set; }
+    }
+}
