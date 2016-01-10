c051ddbd4afd5046d34a15073fa241e3dd6528da
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 14:19:13 +0100

    Added the versioned event interface

---
+++ Darjeel/Darjeel.Infrastructure/EventSourcing/IVersionedEvent.cs
@@ -0,0 +1,9 @@
+﻿using Darjeel.Infrastructure.Messaging;
+
+namespace Darjeel.Infrastructure.EventSourcing
+{
+    public interface IVersionedEvent : IEvent
+    {
+        int Version { get; }
+    }
+}
