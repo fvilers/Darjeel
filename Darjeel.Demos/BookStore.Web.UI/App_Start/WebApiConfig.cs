be82fba4e43cfe50867f7ca1f42bd0519daa60a6
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-10 15:09:14 +0100

    Added the WebApi configuration to the book store demo

---
+++ Darjeel.Demos/BookStore.Web.UI/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using Darjeel.Infrastructure.Serialization.Extensions;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace BookStore.Web.UI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.EnsureFormatting();
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
---
+++ Darjeel.Demos/BookStore.Web.UI/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http;
+
+namespace BookStore.Web.UI
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
