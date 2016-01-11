8c2ecb49114204e5dde3ca2ea3c2ab1b1b4b8507
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-11 15:41:46 +0100

    Added the memory infrastructure assembly

---
+++ Darjeel.Demos/Darjeel.Infrastructure.Memory/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Darjeel.Infrastructure.Memory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dev One")]
+[assembly: AssemblyProduct("Darjeel.Infrastructure.Memory")]
+[assembly: AssemblyCopyright("Copyright © Dev One 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e3cb8b1c-316b-44f1-8eb7-4abec09f14b8")]
