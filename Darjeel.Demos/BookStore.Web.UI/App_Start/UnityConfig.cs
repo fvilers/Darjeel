3c0c765feaec68da7c793787ffc663a9697a6d30
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-12 09:24:34 +0100

    Updated unity for aggregate root repository depencies in the book store demo

--- Darjeel.Demos/BookStore.Web.UI/App_Start/UnityConfig.cs
+++ Darjeel.Demos/BookStore.Web.UI/App_Start/UnityConfig.cs
@@ -1,11 +1,15 @@
 using BookStore.Catalog.CommandHandlers;
 using BookStore.Catalog.Persistence.ReadModels;
 using BookStore.Catalog.ReadModels;
+using Darjeel.Infrastructure.Domain;
+using Darjeel.Infrastructure.EntityFramework.EventSourcing;
+using Darjeel.Infrastructure.EventSourcing;
 using Darjeel.Infrastructure.Memory.Messaging;
 using Darjeel.Infrastructure.Memory.Processors;
 using Darjeel.Infrastructure.Messaging;
 using Darjeel.Infrastructure.Messaging.Handling;
 using Darjeel.Infrastructure.Processors;
+using Darjeel.Infrastructure.Serialization;
 using Microsoft.Practices.Unity;
 using System;
 using System.Collections.Concurrent;
@@ -43,19 +47,27 @@ namespace BookStore.Web.UI
             // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
             // container.LoadConfiguration();
 
-            // Catalog command handlers
+            // Catalog - Command handlers
             container.RegisterType<ICommandHandler, ProductCommandHandler>("ProductCommandHandler");
 
-            // Catalog read models
+            // Catalog - Read models
             container.RegisterType<IReadModelContext, ReadModelContext>();
             container.RegisterType<IReadModelProductDao, ReadModelProductDao>();
 
             // Infrastructure
+            container.RegisterType<ITextSerializer, JsonTextSerializer>();
+
+            // Infrastructure - Event sourcing
+            container.RegisterType<IEventStore, EventStore>();
+            container.RegisterType(typeof(IAggregateRepository<>), typeof(AggregateRepository<>));
+
+            // Infrastructure - Command handling
             container.RegisterType<ICommandHandlerRegistry, CommandHandlerRegistry>(new ContainerControlledLifetimeManager());
             container.RegisterInstance<IProducerConsumerCollection<Envelope<ICommand>>>(new ConcurrentQueue<Envelope<ICommand>>());
             container.RegisterType<IProcessor, CommandProcessor>("CommandProcessor");
             container.RegisterType<ICommandBus, CommandBus>();
 
+            // Infrastructure - Event handling
             container.RegisterType<IEventHandlerRegistry, EventHandlerRegistry>(new ContainerControlledLifetimeManager());
             container.RegisterInstance<IProducerConsumerCollection<Envelope<IEvent>>>(new ConcurrentQueue<Envelope<IEvent>>());
             container.RegisterType<IProcessor, EventProcessor>("EventProcessor");
---
+++ Darjeel/Darjeel.Infrastructure.EntityFramework/EventSourcing/EventStore.cs
@@ -0,0 +1,20 @@
+﻿using Darjeel.Infrastructure.EventSourcing;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Darjeel.Infrastructure.EntityFramework.EventSourcing
+{
+    public class EventStore : IEventStore
+    {
+        public Task<IEnumerable<StoredEvent>> FindAsync(Guid aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(IEnumerable<StoredEvent> events)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
