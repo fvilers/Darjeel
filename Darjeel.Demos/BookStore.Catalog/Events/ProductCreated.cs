e5d21eb4efa8f87f617a158a095637893be549fe
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-12 09:13:40 +0100

    Added the product created event in the book store demo

---
+++ Darjeel.Demos/BookStore.Catalog/Events/ProductCreated.cs
@@ -0,0 +1,16 @@
+﻿using Darjeel.Infrastructure.Messaging;
+using System;
+
+namespace BookStore.Catalog.Events
+{
+    public class ProductCreated : Event
+    {
+        public string Title { get; private set; }
+
+        public ProductCreated(Guid sourceId, string title)
+            : base(sourceId)
+        {
+            Title = title;
+        }
+    }
+}
