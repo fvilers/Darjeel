d1438beaaad6b742ca807b1f5926ebaf43c55028
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-12 08:19:46 +0100

    Added the create product command to the book store demo

---
+++ Darjeel.Demos/BookStore.Catalog/Commands/CreateProduct.cs
@@ -0,0 +1,9 @@
+﻿using Darjeel.Infrastructure.Messaging;
+
+namespace BookStore.Catalog.Commands
+{
+    public class CreateProduct : Command
+    {
+        public string Title { get; set; }
+    }
+}
