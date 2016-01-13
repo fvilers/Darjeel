1cbad2395c2edf0340170b6f6503bec703aaa747
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 15:01:39 +0100

    Added nuspec for the memory infrastructure assembly + automatic pack on release build

--- Darjeel/Darjeel.Infrastructure.Memory/Properties/AssemblyInfo.cs
+++ Darjeel/Darjeel.Infrastructure.Memory/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Darjeel.Infrastructure.Memory")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Darjeel.Infrastructure.Memory")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dev One")]
 [assembly: AssemblyProduct("Darjeel.Infrastructure.Memory")]
