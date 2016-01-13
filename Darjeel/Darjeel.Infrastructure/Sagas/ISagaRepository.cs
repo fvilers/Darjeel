4c5183940b3a137e9ad719a9796880c2efae5732
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 13:20:22 +0100

    Added a saga repository interface

---
+++ Darjeel/Darjeel.Infrastructure/Sagas/ISagaRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Darjeel.Infrastructure.Sagas
+{
+    public interface ISagaRepository<T> : IDisposable
+        where T : class, ISaga
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate);
+        Task StoreAsync(T saga);
+    }
+}
