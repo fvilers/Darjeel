474502beace61341f33e45a8c47d35a94c2cab16
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 15:37:17 +0100

    Updated book store demo to use bootstrap layout

--- Darjeel.Demos/BookStore.Web.UI/Gulpfile.js
+++ Darjeel.Demos/BookStore.Web.UI/Gulpfile.js
@@ -4,9 +4,13 @@ var sourcemaps = require("gulp-sourcemaps");
 var concat = require("gulp-concat");
 var ngAnnotate = require("gulp-ng-annotate");
 var uglify = require("gulp-uglify");
+var minifyCss = require("gulp-minify-css");
+var concatCss = require('gulp-concat-css');
 var config = {
     js: {
         src: [
+            "bower_components/jquery/dist/jquery.js",
+            "bower_components/bootstrap/dist/js/bootstrap.js",
             "bower_components/angular/angular.js",
             "bower_components/angular-route/angular-route.js",
             "app/catalog/**/*.js",
@@ -16,6 +20,16 @@ var config = {
         bundle: {
             path: "assets/js",
             fileName: "app.min.js"
+        }
+    },
+    css: {
+        src: [
+            "bower_components/bootstrap/dist/css/bootstrap.css",
+            "assets/css/custom.css"
+        ],
+        bundle: {
+            path: "assets/css",
+            fileName: "app.min.css"
         }
     }
 }
@@ -31,7 +45,16 @@ gulp.task("js", function () {
 		.pipe(gulp.dest(config.js.bundle.path));
 });
 
-gulp.task("bundles", ["js"], function () {
+gulp.task("css", function () {
+    return gulp
+		.src(config.css.src)
+		.pipe(concatCss(config.css.bundle.fileName))
+		.pipe(minifyCss())
+		.pipe(gulp.dest(config.css.bundle.path));
+
+});
+
+gulp.task("bundles", ["js", "css"], function () {
 });
 
 gulp.task("watch", function () {
