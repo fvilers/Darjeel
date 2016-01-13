e99546ea077a754815163b113beb125a6d3ad6c9
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 16:18:03 +0100

    Added an azure event store

---
+++ Darjeel/Darjeel.Infrastructure.Azure/EventSourcing/EventEntity.cs
@@ -0,0 +1,54 @@
+﻿using Darjeel.Infrastructure.EventSourcing;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace Darjeel.Infrastructure.Azure.EventSourcing
+{
+    public class EventEntity : TableEntity
+    {
+        public string AggregateType { get; set; }
+        public string Payload { get; set; }
+        public string CorrelationId { get; set; }
+
+        public EventEntity()
+        {
+            // Required for Azure table storage
+        }
+
+        public EventEntity(StoredEvent storedEvent)
+            : this()
+        {
+            AggregateId = storedEvent.AggregateId;
+            AggregateType = storedEvent.AggregateType;
+            Version = storedEvent.Version;
+            Payload = storedEvent.Payload;
+            CorrelationId = storedEvent.CorrelationId;
+        }
+
+        [IgnoreProperty]
+        public Guid AggregateId
+        {
+            get { return Guid.Parse(PartitionKey); }
+            set { PartitionKey = value.ToString(); }
+        }
+
+        [IgnoreProperty]
+        public int Version
+        {
+            get { return Int32.Parse(RowKey); }
+            set { RowKey = value.ToString("D10"); }
+        }
+
+        public StoredEvent ToStoredEvent()
+        {
+            return new StoredEvent
+            {
+                AggregateId = AggregateId,
+                AggregateType = AggregateType,
+                Version = Version,
+                Payload = Payload,
+                CorrelationId = CorrelationId
+            };
+        }
+    }
+}
