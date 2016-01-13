2c7847ff121b61ad19e6d9adee5f915c684caf84
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 16:26:41 +0100

    Added nuspec for the web infrastructure assembly + automatic pack on release build

--- Darjeel/Darjeel.Infrastructure.Web/Properties/AssemblyInfo.cs
+++ Darjeel/Darjeel.Infrastructure.Web/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Darjeel.Infrastructure.Web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Darjeel.Infrastructure.Web")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dev One")]
 [assembly: AssemblyProduct("Darjeel.Infrastructure.Web")]
