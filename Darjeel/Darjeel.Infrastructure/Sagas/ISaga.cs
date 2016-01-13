6f8a07b550a169861f2666fe3a6b2e04013d4b46
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 13:13:12 +0100

    Added the saga interface

---
+++ Darjeel/Darjeel.Infrastructure/Sagas/ISaga.cs
@@ -0,0 +1,12 @@
+﻿using Darjeel.Infrastructure.Messaging;
+using System;
+using System.Collections.Generic;
+
+namespace Darjeel.Infrastructure.Sagas
+{
+    public interface ISaga
+    {
+        Guid Id { get; }
+        IEnumerable<Envelope<ICommand>> Commands { get; }
+    }
+}
