2bb30037f0b0e567d8cb8ca6ef1eab081fefa0d1
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 14:40:31 +0100

    Added nuspec for the EF infrastructure assembly + automatic pack on release build

--- Darjeel/Darjeel.Infrastructure.EntityFramework/Properties/AssemblyInfo.cs
+++ Darjeel/Darjeel.Infrastructure.EntityFramework/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Darjeel.Infrastructure.EntityFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Darjeel.Infrastructure.EntityFramework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dev One")]
 [assembly: AssemblyProduct("Darjeel.Infrastructure.EntityFramework")]
