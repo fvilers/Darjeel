28f9716f498b11c51c3fb9ed73786193d22fe4b4
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-13 16:21:36 +0100

    Added nuspec for the Azure infrastructure assembly + automatic pack on release build

--- Darjeel/Darjeel.Infrastructure.Azure/Properties/AssemblyInfo.cs
+++ Darjeel/Darjeel.Infrastructure.Azure/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Darjeel.Infrastructure.Azure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Darjeel.Infrastructure.Azure")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dev One")]
 [assembly: AssemblyProduct("Darjeel.Infrastructure.Azure")]
