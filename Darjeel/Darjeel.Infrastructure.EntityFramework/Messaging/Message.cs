3de35e1cc6b89d7504088cc95450741903e53271
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-14 07:32:44 +0100

    Added a message structure for EF messaging

---
+++ Darjeel/Darjeel.Infrastructure.EntityFramework/Messaging/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Darjeel.Infrastructure.EntityFramework.Messaging
+{
+    public class Message
+    {
+        public Guid Id { get; private set; }
+        public string Body { get; private set; }
+        public string CorrelationId { get; private set; }
+        public DateTime? DeliveryDate { get; private set; }
+
+        protected Message()
+        {
+            // Required for Entity Framework
+        }
+
+        protected Message(string body, DateTime? deliveryDate = null, string correlationId = null)
+        {
+            Id = Guid.NewGuid();
+            Body = body;
+            DeliveryDate = deliveryDate;
+            CorrelationId = correlationId;
+        }
+    }
+}
