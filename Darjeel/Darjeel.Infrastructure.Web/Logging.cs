9b42c49b2afdbcf3a2037949bd2301068b0fbeef
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-18 09:05:06 +0100

    Added a loggin helper for the web assembly

---
+++ Darjeel/Darjeel.Infrastructure.Web/Logging.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Darjeel.Infrastructure.Web
+{
+    internal static class Logging
+    {
+        private static readonly Lazy<TraceSource> DarjeelWebTraceSource = new Lazy<TraceSource>(() => new TraceSource("Darjeel.Web"));
+
+        public static TraceSource DarjeelWeb => DarjeelWebTraceSource.Value;
+    }
+}
