21d552d12a24982a5915c86f0073466b1d34737a
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-18 08:36:27 +0100

    Added a logging helper

---
+++ Darjeel/Darjeel.Infrastructure/Logging.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Darjeel.Infrastructure
+{
+    internal class Logging
+    {
+        private static readonly Lazy<TraceSource> DarjeelTraceSource = new Lazy<TraceSource>(() => new TraceSource("Darjeel"));
+
+        public TraceSource Darjeel => DarjeelTraceSource.Value;
+    }
+}
