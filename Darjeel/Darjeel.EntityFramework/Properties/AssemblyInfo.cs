016a30520d08e17e72cfdad8efc0edc73605a78d
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-18 09:53:39 +0100

    Removed common assembly + marked nupkg as alpha

--- Darjeel/Darjeel.EntityFramework/Properties/AssemblyInfo.cs
+++ Darjeel/Darjeel.EntityFramework/Properties/AssemblyInfo.cs
@@ -20,3 +20,16 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d840b7f8-c596-4302-bd7b-9318d4ebe944")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
--- Darjeel/Darjeel.Memory/Properties/AssemblyInfo.cs
+++ Darjeel/Darjeel.Memory/Properties/AssemblyInfo.cs
@@ -20,3 +20,16 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e3cb8b1c-316b-44f1-8eb7-4abec09f14b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
