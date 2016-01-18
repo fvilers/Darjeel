65a88130752cf67cbcdb95dee683cec3b05cc72d
Fabian Vilers <fabian.vilers@dev-one.com>
2016-01-18 08:53:29 +0100

    Added a logging helper for the azure assembly

---
+++ Darjeel/Darjeel.Infrastructure.Azure/Logging.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Darjeel.Infrastructure.Azure
+{
+    internal static class Logging
+    {
+        private static readonly Lazy<TraceSource> DarjeelAzureTraceSource = new Lazy<TraceSource>(() => new TraceSource("Darjeel.Azure"));
+
+        public static TraceSource DarjeelAzure => DarjeelAzureTraceSource.Value;
+    }
+}
